617cce45c12e7eda680914a7b2b4c75ac3eb4c39
Vesa <42914624+vesas@users.noreply.github.com>
2024-11-29 00:13:36 +0200

    startup update

--- desktop/src/com/vesas/wfc/DesktopLauncher.java
+++ desktop/src/com/vesas/wfc/DesktopLauncher.java
@@ -3,15 +3,23 @@ package com.vesas.wfc;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
-import com.vesas.wfc.MyGdxGame;
-
-// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
-		config.setForegroundFPS(60);
-		config.setTitle("SimpleWFC");
-		config.setWindowedMode(896, 640);
-		new Lwjgl3Application(new MyGdxGame(), config);
-	}
+
+    public static void main(String[] args) {
+        // if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new MyGdxGame(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+        configuration.setTitle("Simple WFC");
+        configuration.useVsync(true);
+        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+        configuration.setWindowedMode(896, 640);
+        return configuration;
+    }
 }   
